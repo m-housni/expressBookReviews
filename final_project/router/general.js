22cf568658c59b7b6eb39e78e6c40cb6e3455f21
m-housni <mohamedhousni1000k@gmail.com>
2024-12-26 22:34:09 +0100

    Refactor book retrieval routes to use async/await for axios calls and improve error handling

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,7 +28,7 @@ public_users.get('/books',function (req, res) {
 public_users.get('/',async function (req, res) {
   try {
     const response = await axios.get('http://localhost:5000/books');
-    const books = response.data;
+    const books = await response.data;
     return res.status(300).json(books);
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
@@ -40,7 +40,7 @@ public_users.get('/isbn/:isbn',async function (req, res) {
   try {
     const isbn = req.params.isbn;
     const response = await axios.get('http://localhost:5000/books');
-    const books = response.data;
+    const books = await response.data;
     const book = books[isbn];
     if (book) {
       return res.status(200).json(book);
@@ -53,8 +53,10 @@ public_users.get('/isbn/:isbn',async function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
+  const response = await axios.get('http://localhost:5000/books');
+  const books = await response.data;
   const booksByAuthor = Object.values(books).filter(book => book.author === author);
   if (booksByAuthor.length > 0) {
     return res.status(200).json(booksByAuthor);
@@ -64,8 +66,10 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   const title = req.params.title;
+  const response = await axios.get('http://localhost:5000/books');
+  const books = await response.data;
   const booksByTitle = Object.values(books).filter(book => book.title === title);
   if (booksByTitle.length > 0) {
     return res.status(200).json(booksByTitle);
